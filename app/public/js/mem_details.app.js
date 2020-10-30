58a3240e28f456c7447b655626b982f46599f5e3
johkrieg <70536065+johkrieg@users.noreply.github.com>
2020-10-30 17:47:58 -0400

    Some edits to DV.

--- app/public/js/mem_details.app.js
+++ app/public/js/mem_details.app.js
@@ -32,6 +32,17 @@ detailsApp = new Vue({
         }
       });
       return members;
+
+      var date_g = {};
+      this.MemDet.forEach((item) => {
+        if (members[item.Member_Name] == undefined) {
+          members[item.Member_Name] = [];
+          members[item.Member_Name].push(item.date_granted);
+        } else {
+          members[item.Member_Name].push(item.date_granted);
+        }
+      });
+      return date_g;
     }},
   created() {
     this.fetchMemDet();
