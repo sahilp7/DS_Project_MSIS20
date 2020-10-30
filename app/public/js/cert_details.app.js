ee8518ec63a0323d5e049867920ccfe00cdbecc4
johkrieg <70536065+johkrieg@users.noreply.github.com>
2020-10-30 16:36:50 -0400

    created cert details js

---
+++ app/public/js/cert_details.app.js
@@ -0,0 +1,38 @@
+certDetailsApp = new Vue({
+  el: '#certDetailView',
+  data: {
+    CertDet: [{
+      certification_name: '',
+      Member_Name: '',
+      date_granted: '',
+      date_expired: ''
+    }]
+  },
+  methods: {
+    fetchCertDet() {
+      fetch('api/records/cert_details.php')
+        .then(response => response.json())
+        .then(json => {
+          this.CertDet = json;
+          console.log(this.CertDet);
+        });
+    }
+  },
+  computed: {
+    groupedCerts() {
+      var certifications = {};
+      this.CertDet.forEach((item) => {
+        if (certifications[item.certification_name] == undefined) {
+          certifications[item.certification_name] = [];
+          certifications[item.certification_name].push(item.Member_Name)
+        } else {
+          certifications[item.certification_name].push(item.Member_Name);
+        }
+      });
+      return certifications;
+    }
+  },
+  created() {
+    this.fetchCertDet();
+  }
+});
