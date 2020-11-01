b7698e76d70a9cc5eac56afaf619128d77f7694e
chinu318 <chinu318@yahoo.com>
2020-10-31 20:07:56 -0400

    Detailed View functionality edits.

--- app/public/js/mem_details.app.js
+++ app/public/js/mem_details.app.js
@@ -17,26 +17,6 @@ detailsApp = new Vue({
           console.log(this.MemDet);
         });
     },
-    filterTable() {
-      // Variables
-      let dropdown, table, rows, cells, country, filter;
-      dropdown = document.getElementById("countriesDropdown");
-      table = document.getElementById("myTable");
-      rows = table.getElementsByTagName("tr");
-      filter = dropdown.value;
-
-      // Loops through rows and hides those with countries that don't match the filter
-      for (let row of rows) { // `for...of` loops through the NodeList
-        cells = row.getElementsByTagName("td");
-        country = cells[0] || null; // gets the 2nd `td` or nothing
-        // if the filter is set to 'All', or this is the header row, or 2nd `td` text matches filter
-        if (filter === "All" || !country || (filter === country.textContent)) {
-          row.style.display = ""; // shows this row
-        } else {
-          row.style.display = "none"; // hides this row
-        }
-      }
-    }
   },
   computed: {
     groupedMembers() {
@@ -50,19 +30,7 @@ detailsApp = new Vue({
         }
       });
       return members;
-    },
-    groupedDateGranted() {
-      var dategrant = {};
-      this.MemDet.forEach((item) => {
-        if (members[item.Member_Name] == undefined) {
-          members[item.Member_Name] = [];
-          members[item.Member_Name].push(item.date_granted);
-        } else {
-          members[item.Member_Name].push(item.date_granted);
-        }
-      });
-      return dategrant;
-    },
+    }
   },
   created() {
     this.fetchMemDet();
